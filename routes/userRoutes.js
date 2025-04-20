a1180f186c5cc060c9ada9912d5a9a743f531bf0
Khadija Tahira <112818364+KhadijaKT@users.noreply.github.com>
2025-04-20 13:23:25 +0500

    adding profile router and controller

---
+++ routes/userRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const { protect } = require('../middleware/authMiddleware');
+const {
+  getUserProfile,
+  updateUserProfile
+} = require('../controllers/userController');
+
+// Profile routes
+router.route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+
+module.exports = router;
